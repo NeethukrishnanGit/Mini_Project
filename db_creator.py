2b481130b13574ee5c6eecadb0bd30f069089fb2
Krithik2009 <krithik@anorasolutions.com>
2023-06-06 16:29:28 +0530

    Instruments collection is added

--- db_creator.py
+++ db_creator.py
@@ -2,3 +2,67 @@ import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["employee_asset_management"]
+
+Instrument_Collection = mydb["Instruments"]
+
+instruments_dict = {"Instruments": [
+    {
+        "name": "Prober",
+        "type": "A",
+        "description": "This is used to test wafer ",
+        "Available": True,
+        "Check_IN": "12:00 AM",
+        "Check_OUT": "11:00 AM"
+    },
+    {
+        "name": "Handler",
+        "type": "A",
+        "description": "This used to test packaged device and place the component for testing",
+        "Available": True,
+        "Check_IN": "08:24 AM",
+        "Check_OUT": "10:00 AM"
+     },
+    {
+        "name": "Manipulator",
+        "type": "C",
+        "description": "This is used to support and tilt the test head",
+        "Available": True,
+        "Check_IN": "12:00 AM",
+        "Check_OUT": "03:30 PM"
+    },
+    {
+        "name": "Test Head",
+        "type": "B",
+        "description": "This carries some critical sensitive devices",
+        "Available": False,
+        "Check_IN": "01:30 PM",
+        "Check_OUT": "04:00 PM"
+     },
+    {
+        "name": "Tester Rack",
+        "type": "COMBINED",
+        "description": "This contains of bulky instruments",
+        "Available": True,
+        "Check_IN": "10:00 AM",
+        "Check_OUT": "11:30 AM"
+     },
+    {
+        "name": "Device Interface",
+        "type": "COMBINED",
+        "description": "This sits on top of the TEST HEAD",
+        "Available": True,
+        "Check_IN": "03:00 PM",
+        "Check_OUT": "05:00 PM"
+     },
+    {
+        "name": "Instrument Cards",
+        "type": "COMBINED",
+        "description": "This is Fixed inside the TEST HEAD",
+        "Available": False,
+        "Check_IN": "02:00 PM",
+        "Check_OUT": "04:00 PM"
+    }
+]
+}
+
+Instrument_Collection.insert_many(instruments_dict["Instruments"])
